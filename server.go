6e713be9ea881730b953d70f16826384c458b61d
Hippolyte Durix <hdurix@ippon.fr>
2018-05-14 18:50:16 +0200

    Add environment variable checks

--- server.go
+++ server.go
@@ -1,8 +1,9 @@
-package description_keeper
+package main
 
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
@@ -10,7 +11,16 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+    checkEnvironmentVariable("KVSTORE_TOKEN")
+    checkEnvironmentVariable("TELEGRAM_BOT_ID")
+    checkEnvironmentVariable("KVSTORE_COLLECTION_NAME")
 	fmt.Println("server started")
 	http.HandleFunc("/", rootHandler)
 	http.ListenAndServe(":8080", nil)
+}
+
+func checkEnvironmentVariable(name string) {
+    if (os.Getenv(name) == "") {
+        panic("Environment variable " + name + " is not present")
+    }
 }
